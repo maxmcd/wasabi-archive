4358e4d61ba08c41be58bf070b5127378d87077f
maxmcd <max.t.mcdonnell@gmail.com>
2018-11-26 20:37:59 -0500

    Harder print statement

--- cmd/go-print/main.go
+++ cmd/go-print/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"bytes"
+	"crypto/rand"
+	"fmt"
+)
+
 func main() {
-	println("hello")
+	c := 10
+	b := make([]byte, c)
+	_, err := rand.Read(b)
+	if err != nil {
+		fmt.Println("error:", err)
+		return
+	}
+	// The slice should now contain random bytes instead of only zeroes.
+	fmt.Println(bytes.Equal(b, make([]byte, c)))
 }
