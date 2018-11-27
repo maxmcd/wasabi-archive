bdf692bdc5d5b807346b6b0ed0d9e9ae2ca3ac68
maxmcd <max.t.mcdonnell@gmail.com>
2018-11-26 20:37:49 -0500

    Running hello world in Go

--- cmd/runtime/main.go
+++ cmd/runtime/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
+	"encoding/binary"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/perlin-network/life/compiler"
 	"github.com/perlin-network/life/exec"
@@ -29,13 +33,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("go")
 	vm, err = exec.NewVirtualMachine(input, exec.VMConfig{}, new(Resolver), &compiler.SimpleGasPolicy{})
 	if err != nil { // if the wasm bytecode is invalid
 		log.Fatal(err)
 	}
-	fmt.Println("go")
-	fmt.Println(vm.GetFunctionExport("run"))
 	code, ok := vm.GetFunctionExport("run")
 	if ok != true {
 		log.Fatal("no run function")
@@ -45,39 +46,108 @@ func main() {
 	// vm.FunctionCode[0].
 }
 
+// VM is an exec.VirtualMachine with utility functions
+type VM struct {
+	exec.VirtualMachine
+}
+
+func getInt(in []byte, out interface{}) {
+	buf := bytes.NewReader(in)
+	err := binary.Read(buf, binary.LittleEndian, out)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func writeInt(in interface{}) *bytes.Buffer {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.LittleEndian, in)
+	if err != nil {
+		panic(err)
+	}
+	return buf
+}
+
+func getInt64(mem []byte, addr int64) int64 {
+	var out int64
+	getInt(mem[addr:addr+8], &out)
+	return out
+}
+
+func getInt32(mem []byte, addr int64) int32 {
+	var out int32
+	getInt(mem[addr:addr+4], &out)
+	return out
+}
+
+func setInt64(mem []byte, addr int64, in int64) int32 {
+	buf := writeInt(in)
+	_, err := buf.Read(mem[addr : addr+8])
+	if err != nil {
+		panic(err)
+	}
+	return 0
+}
+
+func loadString(mem []byte, addr int64) string {
+	saddr := getInt64(mem, addr+0)
+	ln := getInt64(mem, addr+8)
+	return string(mem[saddr : saddr+ln])
+}
+
 // Resolver resolves
 type Resolver struct{}
 
 // ResolveFunc resolverfuncs
 func (r *Resolver) ResolveFunc(module, field string) exec.FunctionImport {
-	log.Println("func", module, field)
+	log.Println("registering func", module, field)
 	switch module {
 	case "go":
 		switch field {
 		case "debug":
 			return func(vm *exec.VirtualMachine) int64 {
 				vm.PrintStackTrace()
-				fmt.Println(vm.GetCurrentFrame().Regs)
-				fmt.Println(vm.GetCurrentFrame().Locals)
-				fmt.Println(vm.CurrentFrame)
-				fmt.Println(vm.Memory[vm.CurrentFrame-8 : vm.CurrentFrame])
+				return 0
+			}
+		case "syscall/js.valueGet":
+			return func(vm *exec.VirtualMachine) int64 {
+				sp := vm.GetCurrentFrame().Locals[0]
+				fmt.Println("valueGet key: ", loadString(vm.Memory, sp+16))
+				return 0
+			}
+		case "runtime.getRandomData":
+			return func(vm *exec.VirtualMachine) int64 {
+				sp := vm.GetCurrentFrame().Locals[0]
+				addr := getInt64(vm.Memory, sp+8)
+				ln := getInt32(vm.Memory, sp+16)
+				_, err := rand.Read(vm.Memory[addr : addr+int64(ln)])
+				if err != nil {
+					panic(err)
+				}
 				return 0
 			}
 		case "runtime.wasmExit":
 			return func(vm *exec.VirtualMachine) int64 {
 				sp := vm.GetCurrentFrame().Locals[0]
-				fmt.Println("wasmExit")
-				fmt.Println(vm.Memory[sp+8 : sp+8+4])
+				exitCode := getInt32(vm.Memory, sp+8)
+				if exitCode != 0 {
+					log.Printf("Wasm exited with a non-zero exit code: %d\n", exitCode)
+				}
+				return 0
+			}
+		case "runtime.nanotime":
+			return func(vm *exec.VirtualMachine) int64 {
+				sp := vm.GetCurrentFrame().Locals[0]
+				setInt64(vm.Memory, sp+8, time.Now().UnixNano())
 				return 0
 			}
 		case "runtime.wasmWrite":
 			return func(vm *exec.VirtualMachine) int64 {
-				// vm.PrintStackTrace()
 				sp := vm.GetCurrentFrame().Locals[0]
-				fmt.Println(vm.Memory[sp+8 : sp+8+8])
-				fmt.Println(vm.Memory[sp+8+8 : sp+8+8+8])
-				fmt.Println(vm.Memory[sp+8+8+8 : sp+8+8+8+4])
-				fmt.Println(vm.GetCurrentFrame().Regs)
+				_ = getInt64(vm.Memory, sp+8) // file descriptor
+				addr := getInt64(vm.Memory, sp+16)
+				ln := getInt32(vm.Memory, sp+24)
+				fmt.Print(string(vm.Memory[addr : addr+int64(ln)]))
 				return 0
 			}
 		default:
