ffe58309be1d76769be5cf0e5a71a6136cdbb2f4
maxmcd <max.t.mcdonnell@gmail.com>
2018-12-06 07:32:01 -0500

    fix rust by disabling features

--- cmd/rust-basic/src/lib.rs
+++ cmd/rust-basic/src/lib.rs
@@ -11,13 +11,13 @@ cfg_if! {
         static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
     }
 }
-use std::env;
+// use std::env;
 
 #[no_mangle]
 pub extern fn main() -> i32 {
-    let path = env::current_dir().unwrap();
-    println!("The current directory is {}", path.display());
-    println!("Hello macro!");
+    // let path = env::current_dir().unwrap();
+    // println!("The current directory is {}", path.display());
+    // println!("Hello macro!");
     let message = "Hello world!".as_bytes();
     unsafe {
         println(message.as_ptr(), message.len());
